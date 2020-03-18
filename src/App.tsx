a01bf867c6984ec07b3db2a4f305d1e695761996
auroville <auroville.pms@gmail.com>
2020-03-19 01:24:21 +0900

    Change : 컴포넌트에서 함수 타입을 받아오기
    컴포넌트에서 함수 타입을 받아오기 위해 Greetings.tsx와 App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Greetings from './Greetings'
 
 function App() {
-  return <Greetings name="World" optional="end" />;
+  //컴포넌트에서 함수를 받아오기위해 수정
+  const onClick = (name: string) => {
+    console.log(`${name} say hello`);
+  }
+  return <Greetings name="World" optional="end" onClick={onClick} />;
 }
 
 export default App;
--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -5,14 +5,22 @@ type GreetingProps = {
     mark: string;
     // 컴포넌트에 생략할 수있는 props 설정하기
     optional?: string;
+    // 아무것도 리턴하지 않는 onClick 함수 생성
+    onClick: (name: string) => void;
 };
 
-const Greetings = ({ name, mark, optional }: GreetingProps) => (
-    <div>
-        Hello, {name} {mark}
-        {optional && <p>{optional}</p>}
-    </div>
-);
+const Greetings = ({ name, mark, optional, onClick }: GreetingProps) => {
+    const handleClick = () => onClick(name);
+    return (
+        <div>
+            Hello, {name} {mark}
+            {optional && <p>{optional}</p>}
+            <div>
+                <button onClick={handleClick}>Click Me</button>
+            </div>
+        </div>
+    )
+};
 
 
 // default값 설정
